f79e4f0f474ec2b040cedd2df1b5bc329cafa562
Irineu Madazio Neto <netomadazio@hotmail.com>
2022-05-21 18:23:59 -0300

    Projeto Final V.1

--- dags/ETL_Raizen.py
+++ dags/ETL_Raizen.py
@@ -1,118 +1,125 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator 
+from airflow.operators.python import PythonOperator
+#from airflow.operators.bash import BashOperator 
 import pandas as pd
-import pathlib
-import win32api
-import win32con
-import win32com.client
-import pandas as pd
-from os.path import join
-from datetime import datetime as dt
+from openpyxl import load_workbook
+from openpyxl import Workbook
+import os
+import subprocess
+import datetime
+from datetime import datetime
+import locale
 
-def processo_ETL():
-    def enable_vba():
-        key = win32api.RegOpenKeyEx(win32con.HKEY_CURRENT_USER,
-                                    "Software\\Microsoft\\Office\\16.0\\Excel"
-                                    + "\\Security", 0, win32con.KEY_ALL_ACCESS)
-        win32api.RegSetValueEx(key, "AccessVBOM", 0, win32con.REG_DWORD, 1)
+locale.setlocale(locale.LC_ALL, 'pt_PT.UTF-8')
 
-    def load_databases(indexes:list=[0, 2]) -> DataFrame:
-        with open (VBA_FILE_PATH, "r") as f: macro = f.read()
+#convert file xls in xlsx
+
+def convert_xls_in_xlsx():
+    os.system('''libreoffice --headless --invisible --convert-to xlsx ~/workspace/desafio_raizen/raw_data/vendas-combustiveis-m3.xls --outdir ~/workspace/desafio_raizen/raw_data/''')
+
+path = "/home/netomadazio/workspace/desafio_raizen/raw_data/vendas-combustiveis-m3.xlsx"
+sheets = {"DPCache_m3": "oil_derivative", "DPCache_m3_2": "diesel"}
+
+#extract tables oil_derivative and diesel
+
+def extract_tables_file():
+
+    def extract_xlsx_sheet(file_location, sheet, output_filelocation):
+        wb = load_workbook(file_location)
+        sheets = wb.sheetnames
+
+        for s in sheets:
+            if s != sheet:
+                sheet_name = wb.get_sheet_by_name(s)
+                wb.remove_sheet(sheet_name)
         
-        excel = win32com.client.Dispatch("Excel.Application")
-        excel.Visible = False
-        workbook = excel.Workbooks.Open(Filename=EXCEL_FILE_PATH)
+        wb.save(output_filelocation)
+    
+    for i in sheets:
+        extract_xlsx_sheet(path, i , f"{sheets[i]}.xlsx")
 
-        # Insert and run macro
-        excelModule = workbook.VBProject.VBComponents.Add(1)
-        excelModule.CodeModule.AddFromString(macro)
-        excel.Application.Run(MACRO_NAME)
+#Transform DataFrame and Saving in parquet format
 
-        dataframes = []
-        for i in indexes:
-            sheet = workbook.Sheets(f"PIVOT-TABLE-{i}")
-            df = pd.DataFrame(sheet.UsedRange())
+def transform_n_load_dataframe():
+    for i in sheets:
+        
+        # read raw data
 
-            # Using first row as column's names
-            df.rename(columns=df.iloc[0], inplace=True)
-            df.drop(df.index[0], inplace=True)
+        df = pd.read_excel(f"{sheets[i]}.xlsx")
 
-            dataframes.append(df)
-        df = pd.concat(dataframes)
+        # Process column names
 
-        # Close without saving
-        excel.Workbooks(1).Close(SaveChanges=0)
-        excel.Application.Quit()
+        df['product'] = df['COMBUSTÍVEL'].str.split(' \(').str[0]
 
-        return df
+        df['unit'] = df['COMBUSTÍVEL'].str.split(" \(").str[1].str.split("\)").str[0]
 
-    def process_datasets(df:DataFrame) -> DataFrame:
-        df = melt_dataset_columns(df)
-        df = adjust_dates_columns(df)
-        df = ajust_column_names(df)
-        return df
+        df = df.rename(columns={'ESTADO': 'uf'})
 
-    def write_partitioned(df:DataFrame, partitions_path:str, partitions_cols:list) -> DataFrame:
-        df.to_parquet(partitions_path,
-                    partition_cols=partitions_cols)
+        months_dict = {}
+        for j in df.columns:
+            try:
+                month_number = datetime.datetime.strptime(j, "%b").strftime('%m')
+                months_dict[j] = month_number
+            except:
+                pass
+        for j in months_dict:
+            df = df.rename(columns={j: months_dict[i]})
+            
+        # drop useless columns
 
-    def melt_dataset_columns(df:DataFrame) -> DataFrame:
-        columns = list(df.columns)
+        df = df.drop(['COMBUSTÍVEL', 'REGIÃO', 'TOTAL'],axis=1)
 
-        values = columns[5:-1]
-        ids = columns[:2] + columns[3:5]
+        # Unpivot months
 
-        df = df.melt(id_vars=ids, value_vars=values, var_name="MES", value_name="VOLUME")
-        return df
+        df =  pd.melt(df, id_vars=['product', 'uf', 'ANO', 'unit'], var_name=['month'], value_name='volume')
 
-    def adjust_dates_columns(df:DataFrame) -> DataFrame:
-        # Adjusts year and month columns values
-        months = {'Jan': '1', 'Fev': '2', 'Mar': '3', 'Abr': '4',  'Mai': '5',  'Jun': '6',
-                'Jul': '7', 'Ago': '8', 'Set': '9', 'Out': '10', 'Nov': '11', 'Dez': '12'}
+        # Create year-month column
 
-        df = df.replace({'MES': months})
-        df['ANO'] = df['ANO'].astype(int).astype(str)
+        df['year_month'] = df['ANO'].astype(str) + '-' + df['month'].astype(str)
 
-        # Combines year and month values into one column
-        df['year_month'] = pd.to_datetime(df['ANO'] + df['MES'], format='%Y%m').dt.date
-        df.drop(columns=['ANO', 'MES'], inplace=True) # Drops original year and month columns
+        # Create timestamp column
 
-        # Adds column "created_at"
-        df['created_at'] = dt.now()
+        df['created_at'] = datetime.today().replace(microsecond=0)
 
-        return df
+        # Drop not asked columns
 
-    def ajust_column_names(df:DataFrame) -> DataFrame:
-        # Renames columns
-        new_col_names = ['product', 'uf', 'unit', 'volume', 'year_month', 'created_at']
-        df.columns = new_col_names
+        df = df.drop(['ANO', 'month'], axis=1)
 
-        # Reorders columns
-        cols_in_order = ['year_month', 'uf', 'product', 'unit', 'volume', 'created_at']
-        df = df.reindex(columns=cols_in_order)
+        # Fill nans with 0
 
-        return df
+        df = df.fillna(0)
 
-    def main():
-        enable_vba()
-        df = load_databases()
-        df = process_datasets(df)
-        write_partitioned(df, partitions_path='dados_combustiveis/', partitions_cols=['year_month'])
+        # Format dtypes
 
-    BASE_PATH = pathlib.Path().resolve()
+        df['product'] = df['product'].astype(str)
+        df['uf'] = df['uf'].astype(str)
+        df['unit'] = df['unit'].astype(str)
+        df['volume'] = df['volume'].astype(float)
+        df['year_month'] = df['year_month'].astype(str)
+        df['created_at'] = pd.to_datetime(df['created_at'])
 
-    EXCEL_FILE_PATH = join(BASE_PATH, 'vendas-combustiveis-m3.xls')
-    VBA_FILE_PATH = join(BASE_PATH, 'macro.txt')
-    MACRO_NAME = 'ExtractPivotTableData'
+        #saving files in parquet format by product and year-months
 
-    if __name__ == '__main__': main()
+        persist_path = f'{sheets[i]}'
+        df.to_parquet(persist_path, engine='fastparquet', partition_cols=['product', 'year_month'])
 
 with DAG('Teste_ETL_Raízen', start_date = datetime(2022,5,18),
-          schedule_interval = '30 * * * *' , catchup = False) as dag:
+           schedule_interval = '30 * * * *' , catchup = False) as dag:
 
-    processo_ETL = PythonOperator(
-        task_id = 'processo_ETL',
-        python_callable = processo_ETL        
+    convert_xls_in_xlsx = PythonOperator(
+         task_id = 'convert_xls_in_xlsx',
+         python_callable = processo_ETL        
     )
 
-    
+    extract_tables_file = PythonOperator(
+         task_id = 'extract_tables_file',
+         python_callable = extract_tables_file 
+    )
+
+    transform_n_load_dataframe = PythonOperator(
+         task_id = 'convert_xls_in_xlsx',
+         python_callable = transform_n_load_dataframe
+    )
+
+    convert_xls_in_xlsx >> extract_tables_file >> transform_n_load_dataframe
+
